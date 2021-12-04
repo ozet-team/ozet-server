fe41889ebaf1dd4bfc4d46d8fccc22ed15bae604
Bart Kim <jaehwankim07120@gmail.com>
2021-12-05 05:13:30 +0900

    fix: pymysqld settings 에 설치

--- ozet/settings/base.py
+++ ozet/settings/base.py
@@ -15,6 +15,9 @@ import os
 import sys
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
