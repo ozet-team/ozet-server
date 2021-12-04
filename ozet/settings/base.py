92dc42df4a7ab4945cf9965318f4c61ebcd59b0e
Bart Kim <jaehwankim07120@gmail.com>
2021-12-05 02:41:58 +0900

    feat: DB Staging 연결 설정

--- ozet/settings/base.py
+++ ozet/settings/base.py
@@ -92,11 +92,16 @@ WSGI_APPLICATION = "ozet.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db-staging.ozet.app',
+        'USER': 'admin',
+        'PASSWORD': 'ozetword!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
