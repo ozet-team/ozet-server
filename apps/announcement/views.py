8a097db5e46b2d5723fa0542c6bfcda91e6e9414
Bart Kim <jaehwankim07120@gmail.com>
2021-12-11 00:14:46 +0900

    fix: permission read only open

--- apps/announcement/views.py
+++ apps/announcement/views.py
@@ -1,8 +1,10 @@
 from apps.announcement.models import Announcement
 from apps.announcement.serializers import AnnouncementSerializer
 from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class AnnouncementViewSet(ReadOnlyModelViewSet):
+    permission_classes = (IsAuthenticatedOrReadOnly, )
     serializer_class = AnnouncementSerializer
     queryset = Announcement.objects.all()
