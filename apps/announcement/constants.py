c5c7273e7d8a00895cd86a3e2700b471ef772449
김찬혁 <zxccxz620@toss.im>
2021-12-19 00:07:38 +0900

    filterset 추가

---
+++ apps/announcement/constants.py
@@ -0,0 +1,2 @@
+class AppConstants:
+    EMPLOYEE_TYPE_NAMES = ["인턴(스텝)", "매니저", "디자이너", "원장"]
--- apps/announcement/views.py
+++ apps/announcement/views.py
@@ -1,10 +1,12 @@
+from apps.announcement.filtersets import AnnouncementFilterSet
 from apps.announcement.models import Announcement
 from apps.announcement.serializers import AnnouncementSerializer
-from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 
 class AnnouncementViewSet(ReadOnlyModelViewSet):
-    permission_classes = (IsAuthenticatedOrReadOnly, )
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = AnnouncementSerializer
     queryset = Announcement.objects.all()
+    filterset_class = AnnouncementFilterSet
