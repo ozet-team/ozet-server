44c32f23c74ded1f7825174afb0f076119b6adc4
김찬혁 <zxccxz620@toss.im>
2022-01-28 20:40:57 +0900

    user를 자동으로 넣어주게 변경

--- apps/announcement/views.py
+++ apps/announcement/views.py
@@ -23,3 +23,6 @@ class BookmarkViewSet(ModelViewSet):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
