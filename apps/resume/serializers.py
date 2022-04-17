df525a7fbd6bd065395d3f4a0e9a172c0e1254e1
Bart Kim <jaehwankim07120@gmail.com>
2022-04-17 20:05:52 +0900

    feat: pdf option added

--- apps/resume/serializers.py
+++ apps/resume/serializers.py
@@ -172,12 +172,28 @@ class ResumePDFSerializer(ModelSerializer):
 
         resume: Resume = request
         html = self.validated_data.get('html')
+        options = {
+            'page-size': 'A4',
+            'margin-top': '0.40in',
+            'margin-bottom': '0.0in',
+            'margin-right': '0in',
+            'margin-left': '0in',
+            'encoding': "UTF-8",
+            'custom-header': [
+                ('Accept-Encoding', 'gzip')
+            ],
+            'cookie': [
+                ('cookie-name1', 'cookie-value1'),
+                ('cookie-name2', 'cookie-value2'),
+            ],
+            'no-outline': None
+        }
 
         css = ".misc/pdf/resume/style.css"
         try:
             # html_str = html.file.read().decode('utf')
             html_str = html
-            pdf = pdfkit.from_string(html_str, False, css=css)
+            pdf = pdfkit.from_string(html_str, False, css=css, options=options)
         except Exception as e:
             raise ValueError(f'{e}')
 
